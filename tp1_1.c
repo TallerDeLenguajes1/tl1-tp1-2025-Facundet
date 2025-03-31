f564f91d8c78f4ab1d235687f99dc584c3efc58c
Facundet <facundotrejo2021@gmail.com>
2025-03-31 03:03:33 -0300

    agrego el punto e

--- tp1_1.c
+++ tp1_1.c
@@ -7,7 +7,14 @@ int main(void)
 
     //------ PUNTO E -------
 
-    
+    char frase[] = "soy un puntero";
+    char *puntero = frase;
+
+    printf ("Contenido del puntero: %s\n", puntero);
+    printf ("Direccion de memoria almacenada en el puntero: %p\n",puntero);
+    printf ("Direccion de memoria de la variable: %p\n", &frase);
+    printf ("Direccion de memoria del puntero %p\n", &puntero);
+    printf ("Tamanio de la memoria usado por la variable: %zu bytes\n", sizeof(puntero));
 
     return 0;
 }
